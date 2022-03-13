db800567153e72cd1da7597c3558915dd44e7c4d
Redouane Achouri <redouane.a.achouri@gmail.com>
2022-03-13 08:10:07 +0100

    Install React and Material UI

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+// import './App.css';
+
+function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography>Hello, world!</Typography>
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+// A custom theme for this app
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Poppins',
+  },
+  // palette: {
+  //   background: {
+  //     default: '#EBA937', // For buttons ##3779eb
+  //   },
+  // },
+});
+
+export default theme;
