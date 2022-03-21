b063ba16112f0a57aab46d4fedeaa48fbd6bb55b
Redouane Achouri <redouane.a.achouri@gmail.com>
2022-03-21 17:04:31 +0100

    Adjust BaseTile size and set nowrap for flex-wrap

--- src/components/BaseTile.tsx
+++ src/components/BaseTile.tsx
@@ -5,20 +5,18 @@ export default function BaseTile({ children }: { children: React.ReactNode }) {
   return (
     <Box
       sx={{
-        position: 'relative',
-        width: 250,
-        height: 250,
+        width: '15rem',
+        height: '20rem',
         backgroundColor: 'honey.main',
         display: 'flex',
         flexDirection: 'column',
-        flexWrap: 'wrap',
+        flexWrap: 'nowrap',
         justifyContent: 'space-evenly',
         alignContent: 'space-around',
         alignItems: 'center',
         border: '1px solid grey',
         borderRadius: 2,
         '&:hover': {
-          // backgroundColor: 'honey.main',
           opacity: [0.9, 0.9, 0.9],
         },
       }}
