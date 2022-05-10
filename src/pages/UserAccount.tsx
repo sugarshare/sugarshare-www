65dc74f9649db8b9b3d9adc5690b7500940406d4
Redouane Achouri <redouane.a.achouri@gmail.com>
2022-05-10 16:59:22 +0200

    Initial user account page

---
+++ src/pages/UserAccount.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+
+import Navigation from 'components/Navigation';
+
+export default function UserAccount() {
+  return (
+    <Container maxWidth='lg'>
+      <Navigation />
+    </Container>
+  );
+}
