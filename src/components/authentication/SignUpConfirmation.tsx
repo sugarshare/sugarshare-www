0d251385c362846ae876d6bbe5209d9f3528f985
Redouane Achouri <redouane.a.achouri@gmail.com>
2022-06-07 09:48:00 +0200

    Add flex alignment for confirmation components

--- src/components/authentication/SignUpConfirmation.tsx
+++ src/components/authentication/SignUpConfirmation.tsx
@@ -38,48 +38,52 @@ export default function SignUpConfirmation() {
 
   return (
     <AuthenticationBase>
-      <Typography variant='body1' sx={{ marginY: 2 }}>
-        A confirmation link has been sent to your email <strong>{state.email}</strong>.
-        <br />
-        <br />
-        Glad to have you on board 😉!
-      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'no-wrap',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant='body1' sx={{ marginY: 2 }}>
+          A confirmation link has been sent to your email <strong>{state.email}</strong>.
+          <br />
+          <br />
+          Glad to have you on board 🥳🎉!
+        </Typography>
 
-      {
-        state.priceId && (
-          <Box
-            component='form'
-            action={`https://${settings.apiDomainName}/subscription/create-checkout-session`}
-            method='POST'
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              flexWrap: 'no-wrap',
-              justifyContent: 'flex-start',
-              alignItems: 'center',
-              width: '100%',
-            }}
-          >
-            <Input type='hidden' name='priceId' value={state.priceId} />
-            <Input type='hidden' name='email' value={state.email} />
-            <Input type='hidden' name='cancelUrl' value={window.location.href} />
-
-            <LoadingButton
-              variant='contained'
-              type='submit'
-              size='large'
-              fullWidth
-              loading={state.isLoading}
-              sx={{
-                marginY: 2,
-                borderRadius: 2,
-              }}
+        {
+          state.priceId && (
+            <Box
+              component='form'
+              action={`https://${settings.apiDomainName}/subscription/create-checkout-session`}
+              method='POST'
+              sx={{ width: '100%' }}
             >
-              Continue to subscription
-            </LoadingButton>
-          </Box>
-        )
-      }
+              <Input type='hidden' name='priceId' value={state.priceId} />
+              <Input type='hidden' name='email' value={state.email} />
+              <Input type='hidden' name='cancelUrl' value={window.location.href} />
+
+              <LoadingButton
+                variant='contained'
+                type='submit'
+                size='large'
+                fullWidth
+                loading={state.isLoading}
+                sx={{
+                  marginY: 2,
+                  borderRadius: 2,
+                }}
+              >
+                Continue to subscription
+              </LoadingButton>
+            </Box>
+          )
+        }
+      </Box>
+
     </AuthenticationBase>
   );
 }
